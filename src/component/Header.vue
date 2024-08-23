2c1d385477fd65b9cc8fab27cac588334c48f0d3
jsnam <skawotkd12@naver.com>
2024-08-23 17:37:55 +0900

    jsnam

---
+++ src/component/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-dark border-bottom" data-bs-theme="dark">
+    <div class="container-fluid container-header">
+      <a class="navbar-brand" href="#">JSDEV</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler"
+        aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">예제코드</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="#">예제사이트</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">뭘만들지고민중</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="뭔가 검색하는곳" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import '@/assets/css/global.css'
+
+</script>
+
+<style></style>
--- src/views/MAIN/JMAIN_1000.vue
+++ src/views/MAIN/JMAIN_1000.vue
@@ -1,11 +1,20 @@
 <template>
-  bb
+  <Header></Header>
+
+  <section class="container">
+    
+  </section>
+
+  <Footer></Footer>
 </template>
 
 <script setup>
+import '@/assets/css/global.css'
+import Header from '@/component/Header.vue';
+import Footer from '@/component/Footer.vue';
 
-</script >
+
+</script>
 
 <style>
-
 </style>
