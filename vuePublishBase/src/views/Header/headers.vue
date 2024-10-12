3f30f6044b86e8ba28de3f827f57da0d862132f2
jsnam <skawotkd12@naver.com>
2024-10-12 20:47:47 +0900

    jsnam

--- vuePublishBase/src/views/Header/headers.vue
+++ vuePublishBase/src/views/Header/headers.vue
@@ -3,26 +3,9 @@
     <div class="header-title">헤더 유형 컬렉션</div>
 
     <div class="contentsWrap">
-      <header class="header-one">
-        <div class="header-one-title">
-          <!-- 이미지랑 타이틀 둘중 하나 선택 or 둘다선택 -->
-          <img src="" class="header-one-logo" alt="로고이미지" />
-          <div class="header-one-home">Home</div>
-        </div>
-        <nav>
-          <ul class="header-one-menu">
-            <li>Menu1</li>
-            <li>Menu2</li>
-            <li>Menu3</li>
-            <li>Menu4</li>
-            <li>Menu5</li>
-          </ul>
-        </nav>
-      </header>
-
-      <div class="header-two"></div>
-
-      <div class="header-three"></div>
+      <header class="header-one"></header>
+      <header class="header-two"></header>
+      <header class="header-three"></header>
     </div>
   </div>
 </template>
